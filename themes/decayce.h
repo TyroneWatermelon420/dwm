bc842f15793ed0a5e9bb0f83ee72d354fa937774
Stormx920 <eriklrosebrock@tutanota.com>
2023-07-19 00:00:33 +0000

    add new theme

---
+++ themes/decayce.h
@@ -0,0 +1,23 @@
+// base colors.
+static const char background[] = "#0d0f18";
+static const char lightbg[]    = "#11131c";
+static const char foreground[] = "#a5b6cf";
+
+// norm colors.
+static const char black[]   = "#262831";
+static const char yellow[]  = "#ecd3a0";
+static const char aqua[]    = "#7bd9e6";
+static const char green[]   = "#90ceaa";
+static const char cyan[]    = "#93cee9";
+static const char red[]     = "#dd6777";
+static const char magenta[] = "#c296eb";
+static const char blue[]    = "#86aaec";
+static const char white[]   = "#cbced3";
+
+
+static const char col_gray1[]       = "#0d0f18";
+static const char col_gray2[]       = "#262831";
+static const char col_gray3[]       = "#a5b6cf";
+//static const char col_gray4[]   841180    = "#c296eb";
+static const char col_gray4[]       = "#841180";
+static const char col_cyan[]        = "#0d0f18";
