4f70b9ec878b69f9e51a5b9f6dcd19dc7fdc315a
TyroneWatermelon420 <tyronewatermlon@tutanota.com>
2023-08-23 10:50:04 +0200

    Patch for statusbutton

--- dwm.c
+++ dwm.c
@@ -858,7 +858,7 @@ drawbar(Monitor *m)
 				urg & 1 << i);
 		x += w;
 	}
-	w = TEXTW(m->ltsymbol);
+	w = blw = TEXTW(m->ltsymbol);
 	drw_setscheme(drw, scheme[SchemeNorm]);
 	x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
 
