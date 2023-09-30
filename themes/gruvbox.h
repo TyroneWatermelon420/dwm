b061c7f06ec70eabb96e61f138676d0bd2bdc9fe
TyroneWatermelon420 <tyronewatermlon@tutanota.com>
2023-09-30 12:02:30 +0000

    Change sel_border color for gruvbox theme

--- themes/gruvbox.h
+++ themes/gruvbox.h
@@ -3,5 +3,5 @@ static const char norm_bg[] = "#262626";
 static const char norm_border[] = "#665c54";
 
 static const char sel_fg[] = "#262626";
+static const char sel_border[] = "#f59542";
 static const char sel_bg[] = "#FE8019";
-static const char sel_border[] = "#fbf1c7";
